6be69c843482573999ae9f9547effe579e407743
Hampus Eriksson <hampuse1@gmail.com>
2024-09-15 17:56:46 +0200

    V6 done

---
+++ Csharp-videos/Klasser/Lektion6/Lektion6/Lektion6/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Lektion6
+{
+    
+    
+    internal class Program
+    {
+
+        //Basklass / parent-class
+        public class Pet
+        {
+            // Attribut
+            public string name;
+            public string breed;
+            public string color;
+            public int age = 0;
+
+            // Konstruktor
+            public Pet(string name, string breed, string color)
+            {
+                this.name = name;
+                this.breed = breed;
+                this.color = color;
+            }
+
+            public virtual void MakeSound()
+            {
+                Console.WriteLine($"{name} makes a sound!");
+            }
+        }
+
+        public class Dog : Pet
+        {
+            // Specifikt attribut för klassen Dog
+            string favorite_food;
+
+            public Dog(string name, string breed, string color, string favorite_food) : base(name, breed, color)
+            {
+                this.favorite_food = favorite_food;
+            }
+
+            public  override void MakeSound()
+            {
+                Console.WriteLine($"{name} barks!");
+            }
+        }
+
+        public class Cat : Pet
+        {
+            int lives = 9;
+            public Cat(string name, string breed, string color) : base(name, breed, color)
+            {
+
+            }
+
+            public void LoseLife()
+            {
+                lives -= 1;
+
+            }
+
+            public override void MakeSound()
+            {
+                Console.WriteLine($"{name} meows!");
+            }
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Dog my_dog = new Dog("Fido", "Labrador", "Gul", "Hamburger");
+            my_dog.MakeSound();
+
+            Cat my_cat = new Cat("Musse", "Bondkatt", "Svart");
+            my_cat.MakeSound();
+
+            
+        }
+    }
+}
